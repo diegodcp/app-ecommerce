834e0dea294b81aa71004791187e9a1330d21db2
Diego da Costa Porto <diego.matias.da-costa-porto@hpe.com>
2024-07-31 17:32:54 +0200

    header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '1/7': '14%',
+      }
+    },
+    colors: {
+      'primary': {
+        DEFAULT: '#8ACCC7',
+      },
+      'secondary': {
+        DEFAULT: '#efdfde',
+      },
+      'third': {
+        DEFAULT: '#cfe4df',
+      },
+      'fourth': {
+        DEFAULT: '#74A687',
+      },
+      white: '#fff',
+      dark: '#989898',
+    }
   },
   plugins: [],
 }
