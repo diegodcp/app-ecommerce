7dc5aa674bdecc4a21b66cc39458fa8694660c8b
Diego da Costa Porto <diego.matias.da-costa-porto@hpe.com>
2024-07-31 16:36:09 +0200

    Environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    companyIdentifier: '',
+    API_URL: '',
+    CLOUDINARY_CLOUD_NAME: '',
+    CLOUDINARY_API_KEY: '',
+    CLOUDINARY_API_SECRET: '',
+    CLOUDINARY_PRESET: '',
+    CLOUDINARY_API: '',
+    SMALL_IMAGE_URL: '',
+    IMAGE_URL: '',
+    THUMBNAIL_URL: '',
+};
