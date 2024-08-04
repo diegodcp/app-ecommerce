cbc97071d46c600af9a6b3211ecc21853d14d952
Diego da Costa Porto <diego.matias.da-costa-porto@hpe.com>
2024-08-04 17:05:30 +0200

    Adding font-awesome

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { NgOptimizedImage, UpperCasePipe } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnDestroy, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -7,8 +7,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { Company, CompanyImage } from '../../shared/models/company.interface';
 import { GlobalService } from '../../services/global.service';
 import { SEOService } from '../../services/seo..service';
-import { environment } from '../../../environments/environment';
 import { TranslateModule } from '@ngx-translate/core';
+import { Product } from '../../shared/models/product.interface';
+import { ProductService } from '../../services/product.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -27,13 +29,18 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './home.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   private globalService = inject(GlobalService);
   private seoService = inject(SEOService);
+  private productService = inject(ProductService);
+
+  private sub: Subscription;
   
   company: Company;
   companySlides: CompanyImage[] = [];
   companyPhoneFormatted: string = '';
+
+  productList: Product[] = [];
   
   
   ngOnInit(): void {
@@ -43,6 +50,21 @@ export class HomeComponent implements OnInit {
     this.seoService.setTitle(this.company.name);
     this.companySlides = this.company.images?.filter((image: CompanyImage) => image.type == 'SLIDE');
 
+    this.getProductList();
+
+  }
+
+  ngOnDestroy(): void{
+    if(this.sub)
+      this.sub.unsubscribe();
+  }
+
+  getProductList(): void{
+    this.sub = this.productService.getBestSellers().subscribe( res => {
+      if(res){
+        this.productList = res;
+      }
+    });
   }
 
 }
